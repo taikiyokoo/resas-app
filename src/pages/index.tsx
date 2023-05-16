a36acc81e506de3e3816f6296324193ab2604584
taikiyokoo <yokootaiki1126@gmail.com>
2023-05-17 00:11:28 +0900

    人口数の折れ線グラフを動的に生成して表示する #5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+//47色
+const colors = ["#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6", 
+"#E6B333", "#3366E6", "#999966", "#99FF99", "#B34D4D",
+"#80B300", "#809900", "#E6B3B3", "#6680B3", "#66991A", 
+"#FF99E6", "#CCFF1A", "#FF1A66", "#E6331A", "#33FFCC",
+"#66994D", "#B366CC", "#4D8000", "#B33300", "#CC80CC", 
+"#66664D", "#991AFF", "#E666FF", "#4DB3FF", "#1AB399",
+"#E666B3", "#33991A", "#CC9999", "#B3B31A", "#00E680", 
+"#4D8066", "#809980", "#E6FF80", "#1AFF33", "#999933",
+"#FF3380", "#CCCC00", "#66E64D", "#4D80CC", "#9900B3", 
+"#E64D66", "#4DB380", "#FF4D4D", "#99E6E6", "#6666FF"];
+
 
 //都道府県の型
 interface Prefecture {
@@ -7,20 +21,23 @@ interface Prefecture {
   prefName: string;
 }
 
+//ある県の総人口データの型
 interface Population {
   prefCode: number;
-  year: number;
-  value: number;
+  data: {
+    year: number;
+    value: number;
+  }[];
 }
 
 const IndexPage: React.FC = () => {
 
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]); //都道府県の一覧
   const [selectedPrefectures, setSelectedPrefectures] = useState<number[]>([]); //選択された都道府県コードの一覧
-  const [population, setPopulation] = useState<Population[]>([]); //人口データ
+  const [population, setPopulation] = useState<Population[]>([]); //選択された県の総人口データの配列
   const [loading,setLoading] = useState<boolean>(true); //ローディング状態管理
 
-  //都道府県の一覧を取得
+  //都道府県の一覧を取得する関数
   const fetchPrefectures = async () => {
     try{
       const res = await axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
@@ -29,7 +46,7 @@ const IndexPage: React.FC = () => {
       setPrefectures(res.data.result);
 
     }catch(error){ 
-      console.log(error);
+      console.error
     }
     setLoading(false);
   };
@@ -39,7 +56,7 @@ const IndexPage: React.FC = () => {
     fetchPrefectures();
   }, []);
 
-  //都道府県のチェックボックスの選択状態が変更されたとき
+  //都道府県のチェックボックスの選択状態が変更されたときに実行される関数
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const prefCode = Number(event.target.value);
     if (event.target.checked) {
@@ -49,7 +66,7 @@ const IndexPage: React.FC = () => {
     }
   };
 
-  //人口データを取得
+  //ある県の総人口データを取得する関数
   const fetchPopulation = async (prefCode: number) => {
     try{
       const res = await axios.get(
@@ -59,21 +76,23 @@ const IndexPage: React.FC = () => {
         }
       );
 
-      //総人口のデータのみを抽出
-      const data: Population[] = res.data.result.data[0].data.map((data: any) => ({
+      //レスポンスから総人口のデータのみを抽出して代入
+      const data: Population = {
         prefCode: prefCode,
-        year: data.year,
-        value: data.value,
-      }));
-
-      return data //人口データを返す
+        data: res.data.result.data[0].data.map((d: any) => ({
+          year: d.year,
+          value: d.value,
+        }))
+      }
+    
+      return data 
 
     }catch(error){
-      console.log(error);
+      console.error
     }
   };
 
-  //選択された都道府県が変更されたとき
+  //選択された都道府県が変更されたときに実行
   useEffect(() => {
 
     //選択された都道府県がない場合は処理を中断
@@ -83,10 +102,10 @@ const IndexPage: React.FC = () => {
     selectedPrefectures.forEach(async (prefCode) => {
       try{
         const data =  await fetchPopulation(prefCode);
-        setPopulation((prev) => ({ ...prev, data}));
-        console.log(data)
+        //受け取ったある県の総人口データを追加
+        data &&setPopulation((prev) => [...prev, data]);
       }catch(error){
-        console.log(error);
+        console.error
       }
       setLoading(false);
     });
@@ -98,18 +117,45 @@ const IndexPage: React.FC = () => {
 
   return (
     <>
-    {prefectures.map((pref) => (
-        <div key={pref.prefCode}>
-          <label>
-            <input
-              type="checkbox"
-              value={pref.prefCode}
-              onChange={handleCheckboxChange}
-            />
-            {pref.prefName}
-          </label>
-        </div>
-      ))}
+    <div style={{ display: 'flex', justifyContent: 'center',alignItems: 'center',height: '100vh' }}>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: '10px'}}>
+      {prefectures.map((pref) => (
+          <div key={pref.prefCode}>
+            <label>
+              <input
+                type="checkbox"
+                value={pref.prefCode}
+                onChange={handleCheckboxChange}
+              />
+              {pref.prefName}
+            </label>
+          </div>
+        ))}
+    </div>
+      <LineChart
+        width={500}
+        height={300}
+        margin={{
+          left: 100,
+        }}
+      >
+        <CartesianGrid strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {selectedPrefectures.map((prefCode) => (
+          <Line
+            key={prefCode}
+            type="monotone"
+            dataKey="value"
+            data={population.find((pop) => pop.prefCode === prefCode)?.data}
+            name={prefectures.find((pref) => pref.prefCode === prefCode)?.prefName}
+            stroke={colors[prefCode]}
+          />
+        ))}
+      </LineChart>
+    </div>
     </>
   )
 
